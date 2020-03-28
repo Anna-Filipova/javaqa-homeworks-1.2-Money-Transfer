3e335121a6d8641ea84738bbbb3cf98be4dcc3a8
Anna-Filipova <62184331+Anna-Filipova@users.noreply.github.com>
2020-03-28 04:14:19 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int balance = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total = balance + transfer_amount;
+        System.out.println(total);
     }
 }
